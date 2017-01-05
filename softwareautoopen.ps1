e8efd0e5d8419ce8e48e42b3a39d5a36de379024
shawn chan <shawn chan>
2017-01-05 23:20:17 +0800

    add software auto start script

---
+++ softwareautoopen.ps1
@@ -0,0 +1,17 @@
+# qq
+start "C:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
+
+# vmware
+start "C:\Program Files (x86)\VMware\VMware Workstation\vmware.exe"
+echo 2
+# EssentialPIM
+#start C:\Program Files (x86)\EssentialPIMPort7\EssentialPIM.exe
+
+# notepad++
+start "C:\Program Files (x86)\Notepad++\notepad++.exe"
+
+# sourceinsigh
+# it will block the thread, so put it at last
+start "C:\Program Files (x86)\Source Insight 3\Insight3.exe"
+
+exit
